255c8f4e28280f72096ce3af02c359e783e56b91
victor linda_win7 <vicwong999@gmail.com>
2019-05-03 17:00:03 -0700

    waypoints

--- builds/development/waypoint/js/waypoint01.js
+++ builds/development/waypoint/js/waypoint01.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   $('.progress-bar').waypoint(function() {
     $('.progress-bar').css({
-      animation: "animate-positive 10s",
+      animation: "animate-positive 2s",
       opacity: "1"
     });
   }, {
